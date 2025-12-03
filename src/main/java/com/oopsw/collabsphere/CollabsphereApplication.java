d2be4d6f036a7698c8fbda68ba9375bad1fc5546
thanhhh1005-blip <thanhhh1005@gmail.com>
2025-12-03 21:45:15 +0700

    first commit

---
+++ src/main/java/com/oopsw/collabsphere/CollabsphereApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.collabsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabsphereApplication.class, args);
+	}
+
+}
