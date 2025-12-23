3c8ea0cb56a9cecf52b9c9dfa4c5b2e200437bda
hyntrem <183181790+hyntrem@users.noreply.github.com>
2025-12-23 15:27:03 +0700

    refactor: restructure resource service packages and layers

---
+++ services/resource-service/src/main/java/com/collab/resourceservice/repository/ResourceRepository.java
@@ -0,0 +1,23 @@
+package com.collab.resourceservice.repository;
+
+import com.collab.resourceservice.entity.Resource;
+import com.collab.resourceservice.enums.ResourceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    Optional<Resource> findByStoredFileName(String storedFileName);
+
+    List<Resource> findAllByDeletedFalse();
+
+    List<Resource> findByUploadedByAndDeletedFalse(String uploadedBy);
+
+    List<Resource> findByTypeAndDeletedFalse(ResourceType type);
+
+    boolean existsByIdAndDeletedFalse(Long id);
+}
